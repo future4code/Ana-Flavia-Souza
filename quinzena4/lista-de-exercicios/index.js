af8bd744205ca494a522204497b53fe4d61adc56
add-flav1s <aflavia17_@hotmail.com>
2021-01-13 18:59:17 -0300

    Exercicios de interpretacao de codigo, numero 2, resolvido.

--- quinzena4/lista-de-exercicios/index.js
+++ quinzena4/lista-de-exercicios/index.js
@@ -1,2 +1,4 @@
 //Exercícios de interpretação de código//
 // 1- Uma funcao feita para retornar o valor que vc digita no prompt multiplicado pelo valor da cotacao.
+
+// 2- Na variavel novoMontante ele vai pegar o valor de 150 e multiplicar pelo case escolhido, no caso Acoes, retornando o valor de 165. Ja na variavel segundoMontante o case escolhido nao existe e ele retornara o default "TIPO DE INVESTIMENTO INFORMADO INCORRETO".
