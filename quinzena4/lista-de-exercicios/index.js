0a928bb55cda67b6c83d5282265909decf60e4fe
add-flav1s <aflavia17_@hotmail.com>
2021-01-13 19:10:03 -0300

    Exercicios de interpretacao de codigo, numero 3, resolvido.

--- quinzena4/lista-de-exercicios/index.js
+++ quinzena4/lista-de-exercicios/index.js
@@ -2,3 +2,5 @@
 // 1- Uma funcao feita para retornar o valor que vc digita no prompt multiplicado pelo valor da cotacao.
 
 // 2- Na variavel novoMontante ele vai pegar o valor de 150 e multiplicar pelo case escolhido, no caso Acoes, retornando o valor de 165. Ja na variavel segundoMontante o case escolhido nao existe e ele retornara o default "TIPO DE INVESTIMENTO INFORMADO INCORRETO".
+
+// 3- Primeiro console pede a quantidade de numeros dentro da array numeros e ela retorna "Quantidade total de numero 14". Segundo console retorna a quantidade de numeros pares, "6". Terceiro console retorna a quantidade de numero impares, "8".
