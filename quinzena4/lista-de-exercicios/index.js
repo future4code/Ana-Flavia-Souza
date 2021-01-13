e35be1f2b1feeae69d33e4f0ec3bb4f8933a389b
add-flav1s <aflavia17_@hotmail.com>
2021-01-13 19:18:55 -0300

    Exercicios de interpretacao de codigo, numero 4, resolvido.

--- quinzena4/lista-de-exercicios/index.js
+++ quinzena4/lista-de-exercicios/index.js
@@ -4,3 +4,5 @@
 // 2- Na variavel novoMontante ele vai pegar o valor de 150 e multiplicar pelo case escolhido, no caso Acoes, retornando o valor de 165. Ja na variavel segundoMontante o case escolhido nao existe e ele retornara o default "TIPO DE INVESTIMENTO INFORMADO INCORRETO".
 
 // 3- Primeiro console pede a quantidade de numeros dentro da array numeros e ela retorna "Quantidade total de numero 14". Segundo console retorna a quantidade de numeros pares, "6". Terceiro console retorna a quantidade de numero impares, "8".
+
+// 4- numero1 = -10 e o numero2 = 1590
