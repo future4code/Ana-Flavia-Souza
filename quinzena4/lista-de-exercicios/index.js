1fe49e8663652f6f1e8eb94ab003577a5e776e87
add-flav1s <aflavia17_@hotmail.com>
2021-01-14 18:55:00 -0300

    Exercicios de funcoes, numero 1, resolvido.

--- quinzena4/lista-de-exercicios/index.js
+++ quinzena4/lista-de-exercicios/index.js
@@ -1,4 +1,4 @@
-//Exercícios de interpretação de código//
+//---Exercícios de interpretação de código---//
 // 1- Uma funcao feita para retornar o valor que vc digita no prompt multiplicado pelo valor da cotacao.
 
 // 2- Na variavel novoMontante ele vai pegar o valor de 150 e multiplicar pelo case escolhido, no caso Acoes, retornando o valor de 165. Ja na variavel segundoMontante o case escolhido nao existe e ele retornara o default "TIPO DE INVESTIMENTO INFORMADO INCORRETO".
@@ -8,7 +8,7 @@
 // 4- numero1 = -10 e o numero2 = 1590
 
 
-//Exercicios de logica de programacao//
+//---Exercicios de logica de programacao---//
 // 1- map, for e forEach
 //exemplo
 // function logicaOne(){
@@ -28,6 +28,7 @@
 //     console.log(nome)
 //   });
 // }
+
 // 2- a) false b) false c) true d) true e) true
 
 // 3-Não funciona, por que nao foi definido valor nenhum para a variavel quantidadeDeNumerosPares e o while está faltando a informacao para passar para o proximo valor, no caso o indice.
@@ -52,30 +53,52 @@
 // }
 // logicaFour(2, 1, 4)
 
-// 5-
-let numOne = Number(prompt("Insira o primeiro numero:"))
-let numTwo = Number(prompt("Insira o segundo numero:"))
-function numMaior(num1, num2){
-    if(num1 > num2){
-        console.log("O maior é:", num1)
-    }else if(num2 > num1){
-        console.log("O maior é:", num2)
-    }
-}
-function div(num1, num2){
-    let div = num1 / num2
-    if(div % 2 === 0){
-        console.log(num1,"é divisivel por",num2)
-    }else{
-        console.log(num1,"não é divisivel por",num2)
-    }
-}
-function dif(num1, num2){
-    let dif = num1-num2
-    console.log("A diferenca entre os dois numeros são: ",dif)
-}
-console.log("Os numeros são:",numOne, "e", numTwo)
-numMaior(numOne, numTwo)
-div(numOne, numTwo)
-div(numTwo, numOne)
-dif(numOne, numTwo)
+// 5-let numOne = Number(prompt("Insira o primeiro numero:"))
+// let numTwo = Number(prompt("Insira o segundo numero:"))
+// function numMaior(num1, num2){
+//     if(num1 > num2){
+//         console.log("O maior é:", num1)
+//     }else if(num2 > num1){
+//         console.log("O maior é:", num2)
+//     }
+// }
+// function div(num1, num2){
+//     let div = num1 / num2
+//     if(div % 2 === 0){
+//         console.log(num1,"é divisivel por",num2)
+//     }else{
+//         console.log(num1,"não é divisivel por",num2)
+//     }
+// }
+// function dif(num1, num2){
+//     let dif = num1-num2
+//     console.log("A diferenca entre os dois numeros são: ",dif)
+// }
+// console.log("Os numeros são:",numOne, "e", numTwo)
+// numMaior(numOne, numTwo)
+// div(numOne, numTwo)
+// div(numTwo, numOne)
+// dif(numOne, numTwo)
+
+
+//---Exercicios de funcoes---//
+// 1-let array = [4, 13, 20, 19, 44, 29];
+// function numMaiorMenor(m1) {
+//
+//   let maior = Math.max(...m1)
+//   let menor = Math.min(...m1)
+//
+//    let maIndex = m1.indexOf(maior)
+//    let meIndex = m1.indexOf(menor)
+//
+//   m1.splice(maIndex, 1)
+//   m1.splice(meIndex, 1)
+//
+//    let segMaior = Math.max(...m1)
+//    let segMenor = Math.min(...m1)
+//
+//  console.log(segMaior)
+//  console.log(segMenor)
+// }
+//
+// numMaiorMenor(array)
